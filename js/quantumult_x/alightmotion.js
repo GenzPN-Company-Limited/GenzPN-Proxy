59bc3f75cd987aa6618545778e827be7ebfc0a77
GenzPN <nguyenvoanhson75@gmail.com>
2025-01-19 12:44:13 +0700

    Up[

---
+++ js/quantumult_x/alightmotion.js
@@ -0,0 +1,34 @@
+// Quantumult X rewrite rule for Alight Motion Premium
+
+var obj = JSON.parse($response.body);
+
+obj = {
+  "data": {
+    "licenses": [{
+      "productId": "com.alightcreative.motion.sub.yearly",
+      "subscriptionId": "premium_subscription",
+      "active": true,
+      "expiryDate": "2099-12-31T23:59:59Z",
+      "type": "subscription",
+      "platform": "ios",
+      "status": "active"
+    }],
+    "accountStatus": {
+      "active": true,
+      "type": "premium",
+      "expiry": "2099-12-31T23:59:59Z",
+      "isFreeTrial": false,
+      "tier": "pro"
+    },
+    "appBuild": 767,
+    "appVersion": "6.2.10",
+    "deviceModel": "iPhone15,3",
+    "platform": "ios",
+    "language": "vi",
+    "country": "VN"
+  },
+  "success": true,
+  "statusCode": 200
+};
+
+$done({body: JSON.stringify(obj)});

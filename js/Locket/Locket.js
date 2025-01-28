fa4e1fda29e618b7ebe2d78cfb74756caa21135c
GenzPN <39546317+GenzPN@users.noreply.github.com>
2025-01-28 13:29:11 +0700

    Update Locket.js

--- js/Locket/Locket.js
+++ js/Locket/Locket.js
@@ -18,7 +18,7 @@ var subscriptionDetails = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2024-07-28T01:04:18Z",
+  original_purchase_date: "2025-01-28T01:04:18Z",
   purchase_date: "2025-01-28T01:04:17Z",
   store: "app_store"
 };

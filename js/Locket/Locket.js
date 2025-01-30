88b39e65b46332344aa5b26de7216e269728339b
GenzPN <39546317+GenzPN@users.noreply.github.com>
2025-01-30 17:05:49 +0700

    upload code

--- js/Locket/Locket.js
+++ js/Locket/Locket.js
@@ -4,11 +4,14 @@ const mapping = {
   'Locket': ['Gold']
 };
 
-// ========= Fixed Variables ========= // 
-// =========  @Ohoang7 ========= // 
+// ========= Fixed Variables ========= //
+// =========  @Ohoang7 ========= //
 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 var obj = JSON.parse($response.body);
+
+// Get current date and time
+var currentDate = new Date().toISOString();
 
 var subscriptionDetails = {
   is_sandbox: false,
@@ -18,14 +21,14 @@ var subscriptionDetails = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2025-01-28T01:04:18Z",
-  purchase_date: "2025-01-28T01:04:17Z",
+  original_purchase_date: currentDate,
+  purchase_date: currentDate,
   store: "app_store"
 };
 
 var entitlementDetails = {
   grace_period_expires_date: null,
-  purchase_date: "2025-01-28T01:04:17Z",
+  purchase_date: currentDate,
   product_identifier: "com.ohoang7.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
@@ -34,7 +37,7 @@ const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
   let [entitlementKey, subscriptionKey] = mapping[match];
-  
+
   if (subscriptionKey) {
     entitlementDetails.product_identifier = subscriptionKey;
     obj.subscriber.subscriptions[subscriptionKey] = subscriptionDetails;

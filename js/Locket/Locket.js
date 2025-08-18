8c027f87454cb0a3f2101e6ded91834b46d3ec74
GenzPN <39546317+GenzPN@users.noreply.github.com>
2025-08-18 21:06:43 +0700

    Update Locket.js

--- js/Locket/Locket.js
+++ js/Locket/Locket.js
@@ -18,14 +18,14 @@ var subscriptionDetails = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2025-08-14T01:04:17Z",
-  purchase_date: "2025-08-14T01:04:17Z",
+  original_purchase_date: "2005-11-01T01:04:17Z",
+  purchase_date: "2005-11-01T01:04:17Z",
   store: "app_store"
 };
 
 var entitlementDetails = {
   grace_period_expires_date: null,
-  purchase_date: "2025-08-14T01:04:17Z",
+  purchase_date: "2005-11-01T01:04:17Z",
   product_identifier: "com.ohoang7.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };

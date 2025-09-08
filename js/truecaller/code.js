f7c91dd2bae6ccf3952ee17fd7d1603686cddc7c
GenzPN <nguyenvoanhson75@gmail.com>
2025-09-08 20:27:43 +0700

    Update Truecaller config and script features
    Expanded the Quantumult_X config URL pattern to match both subscriptions and products/apple endpoints. Updated the Truecaller script to add new features, adjust feature ranks, and modify subscription start and expire dates.

--- js/truecaller/code.js
+++ js/truecaller/code.js
@@ -1,26 +1,28 @@
+// i dont even know what this is but whoever uses this prob finds it cool
 function findUrl(_reg) {
   if (_reg.test($request.url)) {
     return $request.url;
   }
 }
 const features = [
+  { id: "siri_search", rank: 1, status: "Included" },
+  { id: "no_ads", rank: 2, status: "Included" },
   { id: "spam_blocking", rank: -2147483648, status: "Included" },
-  { id: "caller_id", rank: -2147483648, status: "Included" },
-  { id: "no_ads", rank: 1, status: "Included" },
-  { id: "call_assistant", rank: 2, status: "Included" },
   { id: "extended_spam_blocking", rank: 3, status: "Included" },
-  { id: "who_viewed_my_profile", rank: 4, status: "Included" },
-  { id: "incognito_mode", rank: 6, status: "Included" },
-  { id: "premium_badge", rank: 10, status: "Included" },
-  { id: "premium_support", rank: 11, status: "Included" },
-  { id: "gold_caller_id", rank: 13, status: "Included" }
+  { id: "caller_id", rank: -2147483648, status: "Included" },
+  { id: "who_viewed_my_profile", rank: 9, status: "Included" },
+  { id: "incognito_mode", rank: 11, status: "Included" },
+  { id: "premium_badge", rank: 15, status: "Included" },
+  { id: "premium_support", rank: 16, status: "Included" },
+  { id: "premium_app_icon", rank: 19, status: "Included" },
+  { id: "gold_caller_id", rank: 20, status: "Included" } 
 ];
 var obj;
 switch ($request.url) {
   case findUrl(/subscriptions\/status/):
       obj = {
-        expire: "2099-09-06T11:20:25Z",
-        start: "2025-01-28T11:20:25Z",
+        expire: "2050-01-01T01:01:01Z",
+        start: "2024-01-07T02:32:04Z",
         paymentProvider: "Apple",
         isExpired: false,
         isGracePeriodExpired: false,
